b944d31fd62a77a5a476489001619d640f919656
Gundu Pooja <gundupooja1995@gmail.com>
2020-05-05 19:20:06 -0500

    made changes in all files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const createError = require('http-errors');
-const debug = require('debug')('author');
 
 const express = require('express');
+const createError = require('http-errors');
+const debug = require('debug')('author');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -53,7 +53,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'production' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
